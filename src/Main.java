0b7c1c52fd6eabf859753279eaa4bdc04a425c87
deisfansha <deisfansha8082@gmail.com>
2023-07-27 13:43:42 +0700

    Memperbaiki Operator Pembagian

--- src/Main.java
+++ src/Main.java
@@ -35,25 +35,27 @@ public class Main {
 
             System.out.println("Masukkan Operator :");
             String operator = input.next();
-                //kondisi jika inputan operator salah
+                //kondisi jika memasukkan operator
                 if (operator.equalsIgnoreCase("*")){
-                    hasil =  String.valueOf(firstNumber * secondNumber);
+                    hasil =  String.valueOf(firstNumber * secondNumber); // Perkalian
                 }else if(operator.equalsIgnoreCase("+")){
-                    hasil =  String.valueOf(firstNumber + secondNumber);
+                    hasil =  String.valueOf(firstNumber + secondNumber); // Penjumlahan
                 }else if(operator.equalsIgnoreCase("-")){
-                    hasil =  String.valueOf(firstNumber - secondNumber);
+                    hasil =  String.valueOf(firstNumber - secondNumber); // Pengurangan
                 }else if(operator.equalsIgnoreCase("/")){
+                    // Kondisi jika inputan pada pembagian berangka 0
                     if (secondNumber == 0 || firstNumber==0){
                         hasil = "Pembagian angka tidak boleh 0";
                     }else {
-                        hasil =  String.valueOf(firstNumber + secondNumber);
+                        hasil =  String.valueOf(firstNumber / secondNumber); // Pembagian
                     }
                 }
+                // Kondisi jika operator selain tanda *, /, +, -
                 else if (!(operator.equalsIgnoreCase("*")||operator.equalsIgnoreCase("+")||operator.equalsIgnoreCase("-")||operator.equalsIgnoreCase("/"))){
                     hasil = "Operator tidak valid! Masukkan operator yang benar.";
                 }
 
-            System.out.print("Hasil :" +hasil);
+            System.out.print("Hasil :" +hasil); //
 
         }
 
